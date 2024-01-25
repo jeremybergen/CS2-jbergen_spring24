353d597753d41f47fc3e00c727ea672ff09cdc97
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-01-25 10:19:19 -0700

    Wednesday 0124

---
+++ lecture/calc.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+int addNums(int, int);
+int subNums(int, int);
+
+int subNums(int n1, int n2)
+{
+    return n1-n2;
+}
+
+int addNums(int n1, int n2)
+{
+    return n1 + n2;
+}
+
+void sayHello()
+{
+    cout << "Hello" << endl;
+}
---
+++ lecture/headers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include "calc.cpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    int num1, num2;
+
+    sayHello();
+
+    // cout << "Enter two integers separated by a space: ";
+    // cin >> num1 >> num2;
+
+    // cout << num1 << " + " << num2 << " = " << addNums(num1, num2) << endl;
+    // if(argc > 1 && (string)argv[1] == "test") {
+    //     cout << "Inside if" << endl;
+    //     return 0;
+    // }
+    // cout << "outside if" << endl;
+    // cout << "argc: " << argc << endl;
+    // for(int i = 0; i < argc; i++) {
+    //     cout << "argv[" << i << "]: "
+    //          << argv[i] << endl;
+    // }
+    // cout << "Hello World" << endl;
+    return 0;
+}
+
