29d6b94c07ec9664cdba34f96a043802daba5770
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-01-30 21:53:48 -0700

    wednesday 0131

--- lecture/fractions.cpp
+++ lecture/fractions.cpp
@@ -4,6 +4,7 @@ Name: Jeremy Bergen
 Fraction Calculator
 */
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -19,15 +20,110 @@ class Fraction {
     void setNumerator(int);
     void setDenom(int);
     //getters
+    int getNumerator();
+    int getDenom();
+
+    //operator overloading
+
+    Fraction operator+(const Fraction&);
+    Fraction operator-(const Fraction&);
+    friend ostream& operator<<(ostream&, const Fraction&);
 };
+
+Fraction addFractions(Fraction, Fraction);
 
 int main(int argc, char* argv[])
 {
+    char ch;
+    int num, den;
     Fraction frac1;
     Fraction frac2;
+    Fraction frac3;
 
+    Fraction addedFrac;
+    Fraction subFrac;
+
+    cout << "Enter the numerator and denominator for fraction 1: ";
+    cin >> num >> ch >> den;
+    frac1.setNumerator(num);
+    frac1.setDenom(den);
+
+    cout << "Enter the numerator and denominator for fraction 2: ";
+    cin >> num >> ch >> den;
+    frac2.setNumerator(num);
+    frac2.setDenom(den);
+
+    cout << "Enter the numerator and denominator for fraction 3: ";
+    cin >> num >> ch >> den;
+    frac3.setNumerator(num);
+    frac3.setDenom(den);
+
+    // addedFrac = addFractions(frac1, addFractions(frac2, frac3));
+    addedFrac = frac1 + (frac2 + frac3);
+
+    subFrac = frac1 - frac2;
+
+    cout << frac1 << " + " 
+         << frac2 << " + "
+         << frac3 << " = "
+         << addedFrac.getNumerator() << "/" << addedFrac.getDenom() << endl;
+
+    cout << frac1 << " - " 
+         << frac2 << " = "
+         << subFrac.getNumerator() << "/" << subFrac.getDenom() << endl;
 
     return 0;
+}
+
+ostream& operator<<(ostream& os, const Fraction& f1)
+{
+    os << f1._numerator << '/' << f1._denom;
+    return os;
+}
+
+Fraction Fraction::operator-(const Fraction& f1)
+{
+    Fraction subFrac;
+    int commonDenom;
+    int n1, n2;
+    commonDenom = _denom * f1._denom;
+    n1 = _numerator * f1._denom;
+    n2 = f1._numerator * _denom;
+
+    subFrac.setNumerator(n1 - n2);
+    subFrac.setDenom(commonDenom);
+
+    return subFrac;
+}
+
+Fraction Fraction::operator+(const Fraction& f1)
+{
+    Fraction addedFrac;
+    int commonDenom;
+    int n1, n2;
+    commonDenom = _denom * f1._denom;
+    n1 = _numerator * f1._denom;
+    n2 = f1._numerator * _denom;
+
+    addedFrac.setNumerator(n1 + n2);
+    addedFrac.setDenom(commonDenom);
+
+    return addedFrac;
+}
+
+Fraction addFractions(Fraction f1, Fraction f2)
+{
+    Fraction addedFrac;
+    int commonDenom = 0;
+    int num1, num2;
+    commonDenom = f1.getDenom() * f2.getDenom();
+    num1 = f1.getNumerator() * f2.getDenom();
+    num2 = f2.getNumerator() * f1.getDenom();
+
+    addedFrac.setNumerator(num1 + num2);
+    addedFrac.setDenom(commonDenom);
+
+    return addedFrac;
 }
 
 Fraction::Fraction()
@@ -49,8 +145,18 @@ void Fraction::setDenom(int denom)
 {
     while(denom == 0)
     {
-        cout << "The denominator must not be zero. ";
+        cout << "The denominator must not be zero: ";
         cin >> denom;
     }
     _denom = denom;
+}
+
+int Fraction::getNumerator()
+{
+    return _numerator;
+}
+
+int Fraction::getDenom()
+{
+    return _denom;
 }
