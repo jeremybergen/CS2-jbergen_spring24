dab96d29983b8b51aa5ffacf5b9c6c5c1891b46a
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-03-11 10:48:39 -0600

    monday 0311

--- lecture/recursion/main.cpp
+++ lecture/recursion/main.cpp
@@ -1,18 +1,58 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 // int sum(int);
 // int fib(int);
-void stars(int);
+// void stars(int);
+// void tower(int, char, char, char);
+// bool isPalin(string, int, int);
+bool isPalin(string);
 
 int main(int argc, char* argv[])
 {
     // cout << fib(50) << endl;
     // cout << sum(100) << endl;
-    stars(5);
+    // stars(10);
+    // tower(7, 'A', 'B', 'C');
+    // cout << boolalpha << 
+
+    // bool answer = isPalin("racecar");
+    // if(answer == true)
+    // {
+    //     cout << "true" << endl;
+    // }
+    cout << boolalpha << isPalin("amanaplanacanalpanama") << endl;
+    // cout << noboolalpha;
     return 0;
 }
+
+bool isPalin(string inputStr)
+{
+    if(inputStr.length() <= 1)
+    {
+        return true;
+    }
+    // if(inputStr[0] != inputStr[inputStr.length()-1])
+    if(inputStr.at(0) != inputStr.at(inputStr.length()-1))
+    {
+        return false;
+    }
+    return isPalin(inputStr.substr(1, inputStr.length()-2));
+}
+
+// void tower(int numDisk, char start, char mid, char end)
+// {
+//     if(numDisk == 1)
+//     {
+//         cout << "Move disk 1 from: " << start << " to " << end << endl;
+//         return;
+//     }
+//     tower(numDisk - 1, start, end, mid);
+//     cout << "Move disk " << numDisk << " from " << start << " to " << end << endl;
+//     tower(numDisk - 1, mid, start, end);
+// }
 
 // stars(5)
 // *****
@@ -20,18 +60,27 @@ int main(int argc, char* argv[])
 // ***
 // **
 // *
-void stars(int n)
-{
-    if(n > 0)
-    {
-        for(int i = 0; i < n; i++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-        stars(n-1);
-    }
-}
+// void stars(int n)
+// {
+    
+//     if(n > 0)
+//     {
+//         // stars(n-1);
+//         for(int i = 0; i < n; i++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//         stars(n-1);
+//     }
+// }
+
+// int gcd(int a, int b)
+// {
+//     if(b == 0)
+//         return a;
+//     gcd(b, a%b);
+// }
 
 // int fib(int n)
 // {
